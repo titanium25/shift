7ab8a4f8807e7af9082e9cca9ca7fd20b3e29c0f
Alexander Lazarovich <alex.lazarovichh@gmail>
2022-02-02 20:26:38 +0200

    init

--- src/components/ResponsiveAppBar.js
+++ src/components/ResponsiveAppBar.js
@@ -121,7 +121,8 @@ const ResponsiveAppBar = () => {
                                     src={currentUser.providerData[0].photoURL}
                                     sx={{width: 32, height: 32}}
                                 >
-                                    {getInitials(currentUser.providerData[0].displayName)}
+                                    {/*{getInitials(currentUser.providerData[0].displayName)}*/}
+                                    TS
                                 </Avatar>
                             </IconButton>
                         </Tooltip>
