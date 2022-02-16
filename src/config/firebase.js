9e31c262ddd5826ef44faa4501fe81c43bdef088
Alexander Lazarovich <alex.lazarovichh@gmail>
2022-02-16 17:55:28 +0200

    fav icon

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,8 +2,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/database'
 
-
-const app = firebase.initializeApp({
+const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -12,6 +11,9 @@ const app = firebase.initializeApp({
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-})
+}
+
+const app = firebase.initializeApp(firebaseConfig)
+firebase.analytics()
 export const auth = app.auth();
 export default app;
