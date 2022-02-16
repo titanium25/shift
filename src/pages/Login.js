1cad1f2c7535c4f84dfe9dee2870aa178823b749
Alexander Lazarovich <alex.lazarovichh@gmail>
2022-02-16 17:46:51 +0200

    fav icon

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,93 +1,17 @@
-import React, {useRef, useState} from 'react';
-import {Alert, Button, Card, Container, Form} from 'react-bootstrap';
-import {useAuth} from "../context/AuthContext";
-import {Link, useNavigate} from "react-router-dom";
-import {facebookProvider, googleProvider} from "../config/authMethods";
-import {FacebookLoginButton, GoogleLoginButton} from "react-social-login-buttons";
+import React from 'react';
 import '../assets/login.css';
 import logo from '../assets/img/asml--600.jpg';
-
+import LoginForm from "../components/login/LoginForm";
 
 const Login = () => {
-    const emailRef = useRef();
-    const passwordRef = useRef();
-    const {login, socialLogin} = useAuth();
-    const [error, setError] = useState('');
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            setError('');
-            setLoading(true);
-            await login(emailRef.current.value, passwordRef.current.value);
-            navigate("/");
-        } catch {
-            setError('Failed to log in');
-        }
-        setLoading(false);
-    }
-
-    const handleOnClick = async (provider) => {
-        try {
-            setError('');
-            setLoading(true);
-            await socialLogin(provider);
-            navigate("/");
-        } catch {
-            setError('Failed to log in');
-        }
-        setLoading(false);
-    }
-
-
     return (
         <div className="login--container">
             <div className="login--logo">
                 <img src={logo} alt=""/>
             </div>
             <div>
-                <Container
-                    className="d-flex align-items-center justify-content-center"
-                    style={{minHeight: "100vh"}}
-                >
-                    <div className="w-100" style={{maxWidth: "400px"}}>
-                        <Card>
-                            <Card.Body>
-                                <h2 className="text-center mb-4">Log In</h2>
-                                {error && <Alert variant="danger">{error}</Alert>}
-
-                                <FacebookLoginButton onClick={() => handleOnClick(facebookProvider)}/><br/>
-                                <GoogleLoginButton onClick={() => handleOnClick(googleProvider)}/>
-                                <br/>
-                                <Form onSubmit={handleSubmit}>
-                                    <Form.Group id="email">
-                                        <Form.Label>Email</Form.Label>
-                                        <Form.Control type="email" ref={emailRef} required>
-                                        </Form.Control>
-                                    </Form.Group>
-                                    <Form.Group id="password">
-                                        <Form.Label>Password</Form.Label>
-                                        <Form.Control type="password" ref={passwordRef} required>
-                                        </Form.Control>
-                                    </Form.Group>
-                                    <Button disabled={loading} className="w-100 mt-4" type="submit">
-                                        Log in
-                                    </Button>
-                                </Form>
-                                <div className="w-100 text-center mt-3">
-                                    <Link to="/forgot-password">Forgot Password?</Link>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                        <div className="w-100 text-center mt-2">
-                            Need an account? <Link to="/signup">Sign Up</Link>
-                        </div>
-                    </div>
-                </Container>
+                <LoginForm/>
             </div>
-
         </div>
     );
 };
